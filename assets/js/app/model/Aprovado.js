52606522d60148a8f63cf895446329f47603fe13
Aureliano_Jr <jair_.junior@hotmail.com>
2021-01-15 16:08:48 -0300

    feat(aprovado.js): add Aprovado model

---
+++ assets/js/app/model/Aprovado.js
@@ -0,0 +1,13 @@
+export class Aprovado {
+    constructor(id, inscricao, nome, posicaoAmpla, posicaoAfro, pontuacao, sexo, convocado, convocadolistaAfro) {
+        this.id = id;
+        this.inscricao = inscricao;
+        this.nome = nome;
+        this.posicaoAmpla = posicaoAmpla;
+        this.posicaoAfro = posicaoAfro;
+        this.pontuacao = pontuacao;
+        this.sexo = sexo;
+        this.convocado = convocado;
+        this.convocadolistaAfro = convocadolistaAfro;
+    }
+}
